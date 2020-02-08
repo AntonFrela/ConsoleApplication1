5647f358d8232590fb0e529b5c1d86a93567f832
Radiolocation <Sergeeff.toscha2010@yandex.ru>
2020-02-08 14:53:07 +0300

    добавлен комментарий

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,71 @@
 ﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+//Изменение 1
 #include <iostream>
 using namespace std;
 
+class Auto {
+private:
+    int year, month, day;
+public:
+    Auto() {
+        year = 2000;
+        month = 12;
+        day = 31;
+        get();
+        cout << endl;
+    }
+    void message() {
+        cout << "Class is working!" << endl;
+    }
+
+    void set(int date_year, int date_month, int date_day) {
+        year = date_year;
+        month = date_month;
+        day = date_day;
+    }
+
+    void get() {
+        cout << "Year of this auto is " << year << ", month is " << month << ", day is " << day << endl;
+    }
+};
+class b;
+class a {
+    friend int sum(a, b);
+private:
+    int i;
+public:
+    a() {
+        cout << "Enter i:";
+        cin >> i;
+    }
+};
+class b {
+    friend int sum(a, b);
+private:
+    int y;
+
+public:
+    b() {
+        cout << "Enter y:";
+        cin >> y;
+    }
+};
+
+
+int sum(a first, b second) {
+    return (first.i + second.y);
+}
 int main()
 {
+    //Auto shkoda;
+    //shkoda.message();
+    //shkoda.set(2005, 11, 23);
+    //shkoda.get();
+    a first;
+    b second;
+    cout << sum(first, second);
+
     cout << "Hello World!\n";
 }
 
